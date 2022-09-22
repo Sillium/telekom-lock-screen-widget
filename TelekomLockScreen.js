75639622dbd738a510d0b91d36b4fde3555019c1
Sillium <stoerk@gmx.de>
2022-09-22 22:14:41 +0200

    Update TelekomLockScreen.js

--- TelekomLockScreen.js
+++ TelekomLockScreen.js
@@ -1,130 +1,66 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: pink; icon-glyph: exchange-alt;
-const DEBUG = false
+const DEBUG = false;
 const log = DEBUG ? console.log.bind(console) : function () { };
-
-const remainColor = Color.white()
-const depletedColor = new Color("#333333");
 
 let params = getParams(args.widgetParameter);
 
-// Get Provider image
-let providerImage = await getImage(params.provider + "-lockscreen.png", DEBUG);
+// Get Progress Circle service
+let ProgressCircleService = await getService(
+    "ProgressCircle",
+    "https://gist.githubusercontent.com/Sillium/4210779bc2d759b494fa60ba4f464bd8/raw/9e172bac0513cc3cf0e70f3399e49d10f5d0589c/ProgressCircleService.js",
+    DEBUG
+);
 
 // Get Telekom data service
-let tds = await getTelekomDataService(DEBUG);
-
+let TelekomDataService = await getService(
+    "TelekomData",
+    "https://gist.githubusercontent.com/Sillium/0ef7decee63fc57d52a564d9982c0525/raw/d16253de7b6540eec00b500d0fedfe4567c9da2b/TelekomDataService.js",
+    DEBUG
+);
+let tds = new TelekomDataService();
+await tds.update();
 let percent = (params.show === "used") ? tds.usedPercentage : tds.availablePercentage;
-
 log("Percent: " + percent);
 
-// Create the widget
-const widget = await createWidget(args.widgetParameter);
+const widget = new ListWidget()
 
+let progressStack = await ProgressCircleService.drawArc(widget, percent);
+
+// Get Provider image
+let providerImage = await getImage(params.provider + "-lockscreen.png", DEBUG);
+sf = progressStack.addImage(providerImage);
+sf.imageSize = new Size(26, 26);
+sf.imageOpacity = (tds.source === "CACHE" ) ? 0.4 : 1;
+
+widget.presentAccessoryCircular(); // Does not present correctly
 Script.setWidget(widget);
 Script.complete();
 
-// Preview the widget
-if (!config.runsInWidget) {
-    await widget.presentSmall();
-}
-
-/**
- * Create the widget
- * @param {{widgetParameter: string}} config widget configuration
- */
-async function createWidget(widgetParameter) {
-    log(JSON.stringify(widgetParameter, null, 2));
-
-    const widget = new ListWidget();
-
-    let canvas = drawArc(percent, providerImage);
-    widgetImage = widget.addImage(canvas.getImage());
-    widgetImage.imageSize = new Size(65, 65);
-    widgetImage.imageOpacity = (tds.source === "CACHE" ) ? 0.4 : 1;
-    widgetImage.centerAlignImage();
-
-    return widget;
-}
-
-function sinDeg(deg) {
-	return Math.sin((deg * Math.PI) / 180);
-}
-
-function cosDeg(deg) {
-	return Math.cos((deg * Math.PI) / 180);
-}
-
-function drawArc(percent, image) {
-    const canvSize = 200;
-    const canvas = new DrawContext();
-    canvas.opaque = false;
-    const canvWidth = 18; // circle thickness
-    const canvRadius = 80; // circle radius
-    canvas.size = new Size(canvSize, canvSize);
-    canvas.respectScreenScale = true;
-
-    const deg = Math.floor(percent * 3.6);
-
-	let ctr = new Point(canvSize / 2, canvSize / 2);
-	bgx = ctr.x - canvRadius;
-	bgy = ctr.y - canvRadius;
-	bgd = 2 * canvRadius;
-	bgr = new Rect(bgx, bgy, bgd, bgd);
-
-	canvas.opaque = false;
-
-	canvas.setFillColor(remainColor);
-	canvas.setStrokeColor(depletedColor);
-	canvas.setLineWidth(canvWidth);
-	canvas.strokeEllipse(bgr);
-
-	for (t = 0; t < deg; t++) {
-		rect_x = ctr.x + canvRadius * sinDeg(t) - canvWidth / 2;
-		rect_y = ctr.y - canvRadius * cosDeg(t) - canvWidth / 2;
-		rect_r = new Rect(rect_x, rect_y, canvWidth, canvWidth);
-		canvas.fillEllipse(rect_r);
-	}
-
-    const canvLabelRect = new Rect(
-		canvSize / 2 - 50,
-		canvSize / 2 - 50,
-		100,
-		100
-	);
-
-    i = canvas.drawImageInRect(image, canvLabelRect);
-
-    return canvas;
-}
-
 // get library from local filestore or download it once
-async function getTelekomDataService(forceDownload) {
+async function getService(name, url, forceDownload) {
     const fm = FileManager.local();
     const scriptDir = module.filename.replace(fm.fileName(module.filename, true), '');
-    let serviceDir = fm.joinPath(scriptDir, "lib/service/TelekomData");
+    let serviceDir = fm.joinPath(scriptDir, "lib/service/" + name);
 
     if (!fm.fileExists(serviceDir)) {
         fm.createDirectory(serviceDir, true);
     }
 
-    let libFile = fm.joinPath(scriptDir, "lib/service/TelekomData/index.js");
+    let libFile = fm.joinPath(scriptDir, "lib/service/" + name + "/index.js");
     
     if (fm.fileExists(libFile) && !forceDownload) {
         fm.downloadFileFromiCloud(libFile);
     } else {
         // download once
-        let indexjs = await loadText("https://gist.githubusercontent.com/Sillium/0ef7decee63fc57d52a564d9982c0525/raw/d16253de7b6540eec00b500d0fedfe4567c9da2b/TelekomDataService.js");
+        let indexjs = await loadText(url);
         fm.write(libFile, indexjs);
     }
 
-    let TelekomDataService = importModule("lib/service/TelekomData");
-    let tds = new TelekomDataService();
+    let service = importModule("lib/service/" + name);
 
-    await tds.update();
-
-    return tds;
+    return service;
 }
 
 // get images from local filestore or download them once
@@ -146,8 +82,11 @@ async function getImage(name, forceDownload) {
         // download once
         let imageUrl;
         switch (name) {
-            case 'telekom-lockscreen.png':
+            case 'telekomold-lockscreen.png':
                 imageUrl = "https://i.imgur.com/wKlfHIM.png";
+                break;
+            case 'telekom-lockscreen.png':
+                imageUrl = "https://i.imgur.com/Cvzg0ea.png";
                 break;
             case 'congstar-lockscreen.png':
                 imageUrl = "https://i.imgur.com/TsxkIR3.png";
